caac3ebf1bf039cc64d7d71cbb0b3c9753cc5a20
Ivan Valentinov <ivan156drs@gmail.com>
2021-01-16 11:57:09 +0200

    Implemented Cloneable Interface to the Course Class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,6 +27,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+//        Test1();
+        Test2();
+    }
+
+    public static void Test2(){
+        Course c1 = new Course("Mathematics");
+        c1.addStudent("Peter");
+        c1.addStudent("Ivan");
+        c1.addStudent("Georgi");
+        c1.addStudent("Matthew");
+        Course c2 = (Course) c1.clone();
+        c2.dropStudent("Georgi");
+        c2.setCourseName("Informatics");
+        System.out.print("Course 1 - " + c1.getCourseName() + ": ");
+        for(int i=0;i<c1.getNumberOfStudents(); ++i){
+            System.out.print(c1.getStudents()[i] + " ");
+        }
+        System.out.print("\nCourse 2 - " + c2.getCourseName() + ": ");
+        for(int i=0;i<c2.getNumberOfStudents(); ++i){
+            System.out.print(c2.getStudents()[i] + " ");
+        }
+    }
+
+    public static void Test1(){
         Course course1 = new Course("Data Structures");
         Course course2 = new Course("Database Systems");
         course1.addStudent("Peter Jones");
